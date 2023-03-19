90404b20f606dc349f36523361490c57681ec6eb
Muhammad Adil <addhtml00@gmail.com>
2023-03-19 15:10:30 +0500

    cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const fileUpload = require("express-fileupload")
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ credentials: true }));
+app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(fileUpload())
 
